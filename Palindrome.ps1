2c3939281cfb1c39d242a9f5af738f0eecf5df74
Juaninabillion <36932229+Juaninabillion@users.noreply.github.com>
2018-02-28 14:02:40 -0500

    Create Palindrome.ps1

---
+++ Palindrome.ps1
@@ -0,0 +1,30 @@
+function Pal($multiple){
+    $string = $multiple.toString();
+    $bound = $string.length - 1;
+    for($bound;$bound -ge 0 ; $bound--){
+       $compstring+=$string[$bound]
+    }
+     if($string.equals($compstring)){
+          return "$multiple";  }
+        }
+    
+$i = 9999999999
+$j = 999;
+$counter = 0;
+$n  = 0;
+$exit = $fale;
+
+For($i;$i-gt 0;$i--){
+    $counter = 0;    
+    for($counter;$counter-le $n; $counter++){
+        #Write-Host $i  '  ' ($j-$counter)  ' = '  (($i+$counter)*($j-$counter));
+        
+       $exit = ( & Pal(($i+$counter)*($j-$counter)))
+        if($exit -gt 0){
+            $exit
+            exit;
+            }
+    }
+    $n ++;
+}
+
